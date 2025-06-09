f3523d12e1b8bf0870ebf4220a725e440ccf19ef
Koredeee <muhammad.fawwaz003@binus.ac.id>
2025-06-09 08:38:00 +0700

    Initial Commit

---
+++ Alovia/AloviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AloviaApp.swift
+//  Alovia
+//
+//  Created by FWZ on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AloviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
